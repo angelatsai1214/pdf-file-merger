b60fd9aaafc894771ef056b10a7e6be600fd1cc8
Cho Jung (Angela) Tsai <64194102+angelatsai1214@users.noreply.github.com>
2024-10-18 13:03:40 -0700

    initial pdf merger code

---
+++ pdfMerger.py
@@ -0,0 +1,40 @@
+from pypdf import PdfMerger
+import os
+import ssl
+import certifi
+import httpx
+import urllib.request
+
+
+### Function to create SSL context
+def create_ssl_context():
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+    # run `openssl s_client -showcerts -servername api.openai.com -connect api.openai.com:443 </dev/null` and save output to a file `cert.pem` in the same directory as this notebook
+    custom_cert_path = 'cert.pem'
+    if os.path.exists(custom_cert_path):
+        ssl_context.load_verify_locations(custom_cert_path)
+    else:
+        print(f"Warning: Custom certificate file not found at {custom_cert_path}")
+
+    return ssl_context
+
+
+links = ["https://zhiting.ucsd.edu/teaching/dsc190fall2024/assets/lectures/dsc190fall2024-lecture4-llm.pdf", "https://zhiting.ucsd.edu/teaching/dsc190fall2024/assets/lectures/dsc190fall2024-lecture5-ssl.pdf", "https://zhiting.ucsd.edu/teaching/dsc190fall2024/assets/lectures/dsc190fall2024-lecture6-ssl.pdf", "https://zhiting.ucsd.edu/teaching/dsc190fall2024/assets/lectures/dsc190fall2024-lecture7-improving-llm.pdf", "https://zhiting.ucsd.edu/teaching/dsc190fall2024/assets/lectures/dsc190fall2024-lecture8-improving-llm.pdf"]
+pdf = []
+for idx,url in enumerate(links):
+    response = urllib.request.urlopen(url, context=create_ssl_context())
+    file = open(f"pdf{idx}.pdf", 'wb+')
+    file.write(response.read())
+    file.close()
+    pdf.append(f"pdf{idx}.pdf")
+    print("Completed link: ", url)
+    print(f"Saved in file pdf{idx}")
+
+dirpath = '/Users/angtsai/Downloads/UCSD'
+merger = PdfMerger()
+for file in pdf:
+    merger.append(file)
+    os.remove(os.path.join(dirpath, file))
+merger.write("final_res.pdf")
+merger.close()
+
