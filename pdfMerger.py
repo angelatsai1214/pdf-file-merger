6304275626633af1c8d7c955dc48805c7a3851fb
Angela Tsai <angtsai@WKMZT9A55047.global.publicisgroupe.net>
2024-10-19 12:32:04 -0700

    main func with args

--- pdfMerger.py
+++ pdfMerger.py
@@ -4,7 +4,7 @@ import ssl
 import certifi
 import httpx
 import urllib.request
-
+import sys
 
 ### Function to create SSL context
 def create_ssl_context():
@@ -19,22 +19,36 @@ def create_ssl_context():
     return ssl_context
 
 
-links = ["https://zhiting.ucsd.edu/teaching/dsc190fall2024/assets/lectures/dsc190fall2024-lecture4-llm.pdf", "https://zhiting.ucsd.edu/teaching/dsc190fall2024/assets/lectures/dsc190fall2024-lecture5-ssl.pdf", "https://zhiting.ucsd.edu/teaching/dsc190fall2024/assets/lectures/dsc190fall2024-lecture6-ssl.pdf", "https://zhiting.ucsd.edu/teaching/dsc190fall2024/assets/lectures/dsc190fall2024-lecture7-improving-llm.pdf", "https://zhiting.ucsd.edu/teaching/dsc190fall2024/assets/lectures/dsc190fall2024-lecture8-improving-llm.pdf"]
-pdf = []
-for idx,url in enumerate(links):
-    response = urllib.request.urlopen(url, context=create_ssl_context())
-    file = open(f"pdf{idx}.pdf", 'wb+')
-    file.write(response.read())
-    file.close()
-    pdf.append(f"pdf{idx}.pdf")
-    print("Completed link: ", url)
-    print(f"Saved in file pdf{idx}")
+# links = ["https://zhiting.ucsd.edu/teaching/dsc190fall2024/assets/lectures/dsc190fall2024-lecture4-llm.pdf", "https://zhiting.ucsd.edu/teaching/dsc190fall2024/assets/lectures/dsc190fall2024-lecture5-ssl.pdf", "https://zhiting.ucsd.edu/teaching/dsc190fall2024/assets/lectures/dsc190fall2024-lecture6-ssl.pdf", "https://zhiting.ucsd.edu/teaching/dsc190fall2024/assets/lectures/dsc190fall2024-lecture7-improving-llm.pdf", "https://zhiting.ucsd.edu/teaching/dsc190fall2024/assets/lectures/dsc190fall2024-lecture8-improving-llm.pdf"]
 
-dirpath = '/Users/angtsai/Downloads/UCSD'
-merger = PdfMerger()
-for file in pdf:
-    merger.append(file)
-    os.remove(os.path.join(dirpath, file))
-merger.write("final_res.pdf")
-merger.close()
+def main():
+
+    args = sys.argv[1:]
+    links = []
+
+    for l in args:
+        if l[:-3] == "pdf":
+            links.append(l)
+
+    pdf = []
+    for idx,url in enumerate(links):
+        response = urllib.request.urlopen(url, context=create_ssl_context())
+        file = open(f"pdf{idx}.pdf", 'wb+')
+        file.write(response.read())
+        file.close()
+        pdf.append(f"pdf{idx}.pdf")
+        print("Completed link: ", url)
+        print(f"Saved in file pdf{idx}")
+
+    dirpath = '/Users/angtsai/Downloads/UCSD'
+    merger = PdfMerger()
+    for file in pdf:
+        merger.append(file)
+        os.remove(os.path.join(dirpath, file))
+    merger.write("final_res_118.pdf")
+    merger.close()
+    print("Done! Merged pdf result saved in final_res.pdf")
+
+if __name__ == "__main__":
+    main()
 
